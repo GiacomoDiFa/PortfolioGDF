a58aabd67555a0c588e96a27e1b919b30df0d9dd
GiacomoDiFa <giacomodfabrizio@gmai.com>
2024-08-25 12:10:10 +0200

    web

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/PortfolioGDF">
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
